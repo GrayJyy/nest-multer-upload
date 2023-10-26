da76f532d2d215710ecc70eee13720d275d74533
gray_jiang <Gray_Jy@yeah.net>
2023-10-27 02:35:31 +0800

    feat: 上传文件接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,88 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  AnyFilesInterceptor,
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // 单个文件上传
+  @Post('aaa')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: Request['body'],
+  ) {
+    console.log('file', file);
+    console.log('body', body);
+  }
+
+  // 多个文件上传
+  @Post('bbb')
+  @UseInterceptors(
+    FilesInterceptor('bbb', 3, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body: Request['body'],
+  ) {
+    console.log('files', files);
+    console.log('body', body);
+  }
+
+  // 多个文件字段
+  @Post('ccc')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'aaa', maxCount: 2 },
+        { name: 'bbb', maxCount: 3 },
+      ],
+      {
+        dest: 'uploads',
+      },
+    ),
+  )
+  uploadFileFields(
+    @UploadedFiles()
+    files: { aaa?: Express.Multer.File[]; bbb?: Express.Multer.File[] },
+    @Body() body: Request['body'],
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  // 未约定前端传参文件字段名称
+  @Post('ddd')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads',
+    }),
+  )
+  uploadAnyFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body: Request['body'],
+  ) {
+    console.log('body', body);
+    console.log('files', files);
   }
 }
